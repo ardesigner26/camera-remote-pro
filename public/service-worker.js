6e2d63173b7a2765dd88c71c0dabbf2a2ba653c6
Adonias Freitas <ardesigner.26@gmail.com>
2026-06-23 13:04:08 -0300

    Remove broken ISO and exposure controls

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,15 @@
-const CACHE_NAME = 'camera-remote-v1';
-const urlsToCache = [
-  '/',
-  '/mobile.html',
-  '/desktop.html',
-  '/manifest.json'
-];
-
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
